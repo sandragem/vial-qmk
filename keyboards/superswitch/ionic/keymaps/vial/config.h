b37b9391d320755eb2f65cb7e998b92b5de94c78
sandragem <s_moreta@hotmail.com>
2022-07-23 20:32:40 -0400

    Update config.h

---
+++ keyboards/superswitch/ionic/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0xF9, 0x3F, 0x70, 0xDB, 0x2D, 0x93, 0xE3, 0x13}
+#define VIAL_UNLOCK_COMBO_ROWS {1, 1}
+#define VIAL_UNLOCK_COMBO_COLS {6, 7}
